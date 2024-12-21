9e5124f5348d540f3c0309cde21bb5db3652c7ec
felfreitas <soufreitas.felipe@gmail.com>
2024-12-21 10:03:58 -0300

    crud dos livros finalizado

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -4,10 +4,33 @@ import livro from "../models/Livro.js";
 
 class LivroController {
 
+    
     static async listarLivros(req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
+        try {
+
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+
+        } catch (erro) {
+
+            res.status(500).json({ message: `${erro.message} - falha na requisição ` });
+        }
     }
+
+    static async listarLivroPorId(req, res) {
+        try {
+
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+
+            res.status(200).json(livroEncontrado);
+
+        } catch (erro) {
+
+            res.status(500).json({ message: `${erro.message} - falha na requisição do livro` });
+        }
+    }
+
     static async cadastraLivro(req, res) {
         try {
 
@@ -17,26 +40,38 @@ class LivroController {
         } catch (erro) {
 
             res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` });
-            
+
         }
     }
-    static async buscaLivro(req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
-    }
-    // static async listarLivros(req, res){
-    //     const listaLivros = await livro.find({});
-    //     res.status(200).json(listaLivros);
-    // }
-    // static async listarLivros(req, res){
-    //     const listaLivros = await livro.find({});
-    //     res.status(200).json(listaLivros);
-    // }
-    // static async listarLivros(req, res){
-    //     const listaLivros = await livro.find({});
-    //     res.status(200).json(listaLivros);
-    // }
 
+    static async atualizarLivro(req, res) {
+        try {
+
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+
+            res.status(200).json({message:"livro atualizado"});
+
+        } catch (erro) {
+
+            res.status(500).json({ message: `${erro.message} - falha na atualização do livro` });
+        }
+    }
+    static async excluirLivro(req, res) {
+        try {
+
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+
+            res.status(200).json({message:"livro excluído com sucesso"});
+
+        } catch (erro) {
+
+            res.status(500).json({ message: `${erro.message} - falha na exclusão do livro` });
+        }
+    }
+
+  
 
 
     // app.get("/livros/:id", (req, res) => {
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,8 +5,10 @@ import LivroController from "../controllers/livroController.js";
 const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
-routes.get("/livros/:id", LivroController.buscaLivro);
-routes.post("/livros", LivroController.listarLivros);
-routes.put("/livros", LivroController.listarLivros);
-routes.put("/livros", LivroController.listarLivros);
-routes.delete("/livros", LivroController.listarLivros);
+routes.get("/livros/:id", LivroController.listarLivroPorId);
+routes.post("/livros", LivroController.cadastraLivro);
+routes.put("/livros/:id", LivroController.atualizarLivro);
+routes.delete("/livros/:id", LivroController.excluirLivro);
+
+
+export default routes;
